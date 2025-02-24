8656f5e6a537c251da0d49a2449c31744c632ccc
Abdulrahman Shubbak <a.shubbak@outlook.com>
2025-02-24 17:12:42 +0100

    made setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/zathurarc ~/.config/zathura/zathurarc
+ln -sf ~/dotfiles/latexmkrc ~/.latexmkrc
+
+echo "Symlinks created!"
