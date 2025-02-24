e4a6eae1be2e6dd6979ad30951a7f12cc06353ba
Abdulrahman Shubbak <a.shubbak@outlook.com>
2025-02-24 17:30:42 +0100

    added latexmkrc zathurarc zshrc from laptop

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/zathurarc ~/.config/zathura/zathurarc
 ln -sf ~/dotfiles/latexmkrc ~/.latexmkrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 
 echo "Symlinks created!"
