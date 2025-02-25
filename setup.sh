12dc37c869193493be73fff749ccad8b57a79ac4
Abdulrahman Shubbak <a.shubbak@outlook.com>
2025-02-25 15:50:09 +0100

    added powelevel10k rc file

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/zathurarc ~/.config/zathura/zathurarc
 ln -sf ~/dotfiles/latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/p10.zsh ~/.p10k.zsh
 
 echo "Symlinks created!"
