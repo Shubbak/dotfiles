e8f2aa8f14c17a3e6bb74f8980d7331c90b1214c
Abdulrahman Shubbak <a.shubbak@outlook.com>
2025-04-04 12:08:43 +0200

    moved dotfiles repo into ~/Repos

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -sf ~/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/zathurarc ~/.config/zathura/zathurarc
-ln -sf ~/dotfiles/latexmkrc ~/.latexmkrc
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/p10.zsh ~/.p10k.zsh
+ln -sf ~/Repos/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/Repos/dotfiles/zathurarc ~/.config/zathura/zathurarc
+ln -sf ~/Repos/dotfiles/latexmkrc ~/.latexmkrc
+ln -sf ~/Repos/dotfiles/zshrc ~/.zshrc
+ln -sf ~/Repos/dotfiles/p10.zsh ~/.p10k.zsh
 
 echo "Symlinks created!"
