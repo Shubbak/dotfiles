cc8adef0067fe212cf6fbd9139fbd63e52c140e8
Abdulrahman Shubbak <a.shubbak@outlook.com>
2025-04-04 19:30:59 +0200

    added script to quickly install necessary packages

---
+++ setup_symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -sf ~/Repos/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/Repos/dotfiles/zathurarc ~/.config/zathura/zathurarc
+ln -sf ~/Repos/dotfiles/latexmkrc ~/.latexmkrc
+ln -sf ~/Repos/dotfiles/zshrc ~/.zshrc
+ln -sf ~/Repos/dotfiles/p10.zsh ~/.p10k.zsh
+
+echo "Symlinks created!"
