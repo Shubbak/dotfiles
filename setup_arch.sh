c75706806a73b8b05f3583a7cbef6c0a2e58e953
Shubbak <54030905+Shubbak@users.noreply.github.com>
2026-02-16 15:22:09 +0100

    setup_arch.sh  mit yay
    unbedingt prüfen!

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,69 +1,95 @@
 #!/bin/bash
 
-# Update system
-sudo pacman -Syu --noconfirm
-
+# -----------------------------
+# Helper functions
+# -----------------------------
 command_exists() {
     command -v "$1" &>/dev/null
 }
 
-# List of packages (Arch package names)
-list_pacman=(
-    "git"
-    "zsh"
-    "neovim"
-    "latexmk"
-    "ripgrep"
-    "python"
-    "python-virtualenv"
-    "python-numpy"
-    "python-pandas"
-    "python-scipy"
-    "python-neovim"
-    "python-pip"
-    "nodejs"
-    "tree"
-    "neofetch"
-    "zathura"
-    "syncthing"
-    "tldr"
-    "wget"
-    "unzip"
-    "guake"
-    "vlc"
-    "cowsay"
-    "inkscape"
-    "cifs-utils"
-    "pandoc"
+install_yay() {
+    # Install yay if not present
+    if ! command_exists yay; then
+        echo "Installing yay (AUR helper)..."
+        sudo pacman -S --needed --noconfirm git base-devel
+        tmpdir=$(mktemp -d)
+        git clone https://aur.archlinux.org/yay.git "$tmpdir/yay"
+        pushd "$tmpdir/yay" || exit
+        makepkg -si --noconfirm
+        popd || exit
+        rm -rf "$tmpdir"
+        echo "yay installed."
+    fi
+}
+
+# -----------------------------
+# Update system
+# -----------------------------
+echo "Updating system..."
+sudo pacman -Syu --noconfirm
+
+# -----------------------------
+# Pacman packages
+# -----------------------------
+pacman_packages=(
+    git zsh neovim latexmk ripgrep python python-virtualenv python-numpy
+    python-pandas python-scipy python-neovim python-pip nodejs tree
+    neofetch zathura syncthing tldr wget unzip guake vlc cowsay inkscape
+    cifs-utils pandoc
 )
 
-# Check for missing packages
 missing_packages=()
-for pkg in "${list_pacman[@]}"; do
+for pkg in "${pacman_packages[@]}"; do
     if ! pacman -Qi "$pkg" &>/dev/null; then
         missing_packages+=("$pkg")
     fi
 done
 
-# Install missing packages
 if [ ${#missing_packages[@]} -gt 0 ]; then
-    sudo -v
-    echo "Installing missing packages: ${missing_packages[*]}"
+    echo "Installing missing pacman packages: ${missing_packages[*]}"
     sudo pacman -S --noconfirm "${missing_packages[@]}"
 else
-    echo "All packages are installed"
+    echo "All pacman packages are installed."
 fi
 
-# Change default shell to zsh for current user if not already
+# -----------------------------
+# AUR packages
+# -----------------------------
+aur_packages=(
+    zoom anki-bin obsidian zotero
+)
+
+# Ensure yay is installed
+install_yay
+
+missing_aur=()
+for pkg in "${aur_packages[@]}"; do
+    if ! pacman -Qi "$pkg" &>/dev/null && ! yay -Qi "$pkg" &>/dev/null; then
+        missing_aur+=("$pkg")
+    fi
+done
+
+if [ ${#missing_aur[@]} -gt 0 ]; then
+    echo "Installing missing AUR packages: ${missing_aur[*]}"
+    yay -S --noconfirm "${missing_aur[@]}"
+else
+    echo "All AUR packages are installed."
+fi
+
+# -----------------------------
+# Change default shell to zsh
+# -----------------------------
 if [ "$SHELL" != "$(which zsh)" ]; then
     echo "Changing default shell to zsh..."
     chsh -s "$(which zsh)"
-    echo "Shell changed. You may need to log out and log back in for it to take effect."
+    echo "Shell changed. Log out and back in for it to take effect."
 else
     echo "Default shell is already zsh."
 fi
 
-# Install Oh My Zsh non-interactively if not already installed
+# -----------------------------
+# Oh My Zsh installation
+# -----------------------------
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing Oh My Zsh..."
     RUNZSH=no KEEP_ZSHRC=yes \
@@ -73,7 +99,9 @@ else
     echo "Oh My Zsh already installed."
 fi
 
-# Install Powerlevel10k theme if not already present
+# -----------------------------
+# Powerlevel10k theme
+# -----------------------------
 THEME_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 if [ ! -d "$THEME_DIR" ]; then
     echo "Installing Powerlevel10k theme..."
@@ -83,24 +111,22 @@ else
     echo "Powerlevel10k already installed."
 fi
 
-# Ensure Powerlevel10k is set as the active theme in .zshrc
+# Ensure Powerlevel10k is set in .zshrc
 ZSHRC="$HOME/.zshrc"
 if [ -f "$ZSHRC" ]; then
     if grep -q '^ZSH_THEME=' "$ZSHRC"; then
-        # Replace any existing theme line
         sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\\/powerlevel10k"/' "$ZSHRC"
-        echo "Updated ZSH_THEME in .zshrc."
     else
-        # Add theme line near the top if missing
         sed -i '1i ZSH_THEME="powerlevel10k/powerlevel10k"' "$ZSHRC"
-        echo "Added ZSH_THEME to .zshrc."
     fi
 else
-    echo "Creating .zshrc with Powerlevel10k theme..."
     echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' > "$ZSHRC"
 fi
 
-echo "Please make sure Zoom, Anki, Obsidian and Zotero are installed manually."
-echo "Please make sure Telegram, Firefox and Thunderbird are installed with Flatpak."
-echo "Make sure to also clone and run the setup in the nvim repo."
-echo "Make sure to install and use Fira Code Nerd Font."
+# -----------------------------
+# Final notes
+# -----------------------------
+echo "Setup complete!"
+echo "- Make sure Telegram, Firefox and Thunderbird are installed via Flatpak if desired."
+echo "- Ensure Fira Code Nerd Font is installed and configured."
+echo "- Clone and run the setup for your nvim config repository."
